cfefd79eacaceb3f55504647890587b53567617d
wxy <1743657883@qq.com>
2024-04-22 13:03:55 +0800

    新版完成

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
-删除注册表:reg delete "HKLM\SOFTWARE\Classes\db2db" /f
-添加注册表: reg.exe import install.reg
-其他Windows架构（如ARM64）或在非Windows系统上编译Windows EXE文件:
-GOOS=windows GOARCH=amd64 go build -o proxyPassword.exe proxyPassword.go
+## 基础命令
+删除注册表:
+```
+reg delete "HKLM\SOFTWARE\Classes\db2db" /f
+```
+添加注册表:
+```
+reg.exe import install.reg
+```
 基础编译:
+```
 go build -o output.exe main.go
+```
+其他Windows架构（如ARM64）或在非Windows系统上编译Windows EXE文件:
+```
+GOOS=windows GOARCH=amd64 go build -o proxyPassword.exe proxyPassword.go
+```
 
 生成rsrc.syso
 ```
 go get github.com/akavel/rsrc
 rsrc -manifest proxy_password_filling.manifest -o rsrc.syso
+```
+
+
+## 程序使用方法
+### xshell浏览器访问示例:
+```
+proxyPassword://Soft=xshell&Protocol=ssh&Username=root&Password=123&Port=21&Host=127.0.0.1
 ```
--- proxyPassword.go
+++ proxyPassword.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gonutz/w32"
 	"github.com/sirupsen/logrus"
 	"gopkg.in/ini.v1"
+	"net/url"
 	"os"
 	"os/exec"
 	"os/user"
@@ -14,7 +15,7 @@ import (
 
 var log = logrus.New()
 
-// 定义软件
+// 定义软件 \\Mac\Home\Downloads\downloadobject\apipost_win_x64.exe
 var soft = []string{"xshell", "xftp", "filezilla"}
 
 func main() {
@@ -27,74 +28,105 @@ func main() {
 	// 定义配置文件路径
 	configPath := userDir.HomeDir + "\\AppData\\Roaming\\password_proxy\\password_proxy_path.ini"
 	logPath := userDir.HomeDir + "\\AppData\\Roaming\\password_proxy\\password_proxy.log"
-	configPath = "./password_proxy_path.ini"
-	logPath = "./password_proxy.log"
-	// 判断入参是注册还是启动
-	if os.Args == nil {
-		// 初始化日志
-		initLog(logPath)
-		// 操作注册表
-		createRegisterTable()
-		// 创建文件
-		if !fileExists(configPath) {
-			createFileIfNotExist(configPath)
-			// 读取INI文件
-			cfg, err := ini.LoadSources(ini.LoadOptions{
-				SkipUnrecognizableLines: true,
-			}, configPath)
-			if err != nil {
-				errorMessage("无法加载INI文件:" + configPath)
-				os.Exit(0)
-			}
-			// 指定exe程序
-			for _, exe := range soft {
-				cfg.Section("path").Key(exe).SetValue("")
-				if err := cfg.SaveTo(configPath); err != nil {
-					errorMessage("无法保存INI文件: %v" + err.Error())
-					os.Exit(0)
-				}
-			}
-		}
-		successMessage("配置完成,请在配置文件password_proxy_path.ini里填写exe程序地址")
-	} else {
-		if len(os.Args) != 6 {
-			errorMessage("参数信息不正确!")
-		}
-		start(configPath)
+	exePath, err := os.Executable()
+	// 针对mac虚拟机临时处理
+	configPath = strings.Replace(exePath, "proxyPassword.exe", "password_proxy_path.ini", -1)
+	logPath = strings.Replace(exePath, "proxyPassword.exe", "password_proxy.log", -1)
+	if err != nil {
+		errorMessage("获取当前程序路径失败")
+		os.Exit(0)
 	}
+	// 初始化日志
+	initLog(logPath)
+	if len(os.Args) == 1 {
+		initServer(configPath, exePath)
+		return
+	}
+	// 启动程序
+	if len(os.Args) == 7 {
+		start(configPath)
+		return
+	}
+	errorMessage("参数信息不正确!")
+	os.Exit(0)
 }
 
-// 启动软件
+// 初始化服务
+func initServer(configPath string, exePath string) {
+	// 操作注册表
+	createRegisterTable(exePath)
+	// 创建文件
+	if !fileExists(configPath) {
+		createFileIfNotExist(configPath)
+		// 读取INI文件
+		cfg, err := ini.LoadSources(ini.LoadOptions{
+			SkipUnrecognizableLines: true,
+		}, configPath)
+		if err != nil {
+			errorMessage("无法加载INI文件:" + configPath)
+			os.Exit(0)
+		}
+		// 指定exe程序
+		for _, exe := range soft {
+			cfg.Section("path").Key(exe).SetValue("")
+			if err := cfg.SaveTo(configPath); err != nil {
+				errorMessage("无法保存INI文件: %v" + err.Error())
+				os.Exit(0)
+			}
+		}
+	}
+	successMessage("配置完成,请在配置文件password_proxy_path.ini里填写exe程序地址")
+}
+
+type Param struct {
+	Soft     string //软件
+	Protocol string //协议
+	Username string //用户名
+	Password string //密码
+	Port     string //端口
+	Host     string //主机
+}
+
+// 启动软件 proxyPassword://Soft=xshell&Protocol=ftp&Username=root&Password=123&Port=21&Host=127.0.0.1
 func start(configPath string) {
-	// 根据入参启动程序
-	Soft := os.Args[1]
-	Protocol := os.Args[2]
-	Username := os.Args[3]
-	Password := os.Args[4]
-	Host := os.Args[5]
-	Port := os.Args[6]
-	// 通过Soft获取路径
+	// 获取全路径
+	var data = strings.Replace(os.Args[1], "proxypassword://", "", -1)
+	// 解析查询字符串
+	values, err := url.ParseQuery(data)
+	if err != nil {
+		errorMessage("读取程序参数失败:" + err.Error())
+		os.Exit(0)
+	}
+	// 创建Param结构体对象并映射查询参数
+	p := Param{
+		Soft:     values.Get("Soft"),
+		Protocol: values.Get("Protocol"),
+		Username: values.Get("Username"),
+		Password: values.Get("Password"),
+		Port:     values.Get("Port"),
+		Host:     values.Get("Host"),
+	}
 	// 读取INI文件
 	cfg, err := ini.LoadSources(ini.LoadOptions{
 		SkipUnrecognizableLines: true,
 	}, configPath)
 	if err != nil {
-		errorMessage("无法加载INI文件:" + configPath)
+		errorMessage("无法加载INI文件:" + err.Error())
 		os.Exit(0)
 	}
-	kv, err := cfg.Section("path").GetKey(Soft)
+	kv, err := cfg.Section("path").GetKey(p.Soft)
 	if err != nil {
 		errorMessage("读取软件路径失败!" + err.Error())
 	}
 	if !fileExists(kv.Value()) {
 		errorMessage("软件路径不存在:" + kv.Value())
 	}
-	execParam := Protocol + "://" + Username + ":" + Password + "@" + Host + ":" + Port
+	execParam := p.Protocol + "://" + p.Username + ":" + p.Password + "@" + p.Host + ":" + p.Port
 	cmd := exec.Command(kv.Value(), execParam)
 	if err := cmd.Run(); err != nil {
-		log.Println("启动失败:", err)
+		log.Error("启动失败:", err, execParam)
 	} else {
-		log.Println("启动成功!")
+		log.Info("启动成功!", execParam)
 	}
 }
 
@@ -104,20 +136,19 @@ func deleteRegisterTable() {
 	// 隐藏命令行窗口（仅适用于Windows平台）
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	if err := cmd.Run(); err != nil {
-		errorMessage("移除旧的注册表失败:" + err.Error())
-		os.Exit(0)
+		//errorMessage("移除旧的注册表失败:" + err.Error())
+		//os.Exit(0)
+		log.Error("移除旧的注册表失败:" + err.Error())
 	} else {
 		log.Info("移除旧的注册表成功!")
 	}
 }
 
 // 添加注册表
-func createRegisterTable() {
+func createRegisterTable(exePath string) {
 	deleteRegisterTable()
 	var currentPath string
-	var exePath string
 	currentPath, _ = os.Getwd()
-	exePath = "\\\\Mac\\Home\\Desktop\\proxyPassword.exe"
 	if file, err := os.Create("./proxyPassword.reg"); err != nil {
 		errorMessage("创建注册表文件失败:" + err.Error())
 		os.Exit(0)
@@ -128,7 +159,6 @@ func createRegisterTable() {
 		data += "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\proxyPassword]\n"
 		data += "@=\"proxyPassword\"\n"
 		data += "\"URL Protocol\"=\"\"" + "\n\n"
-		//data += strings.Replace(strings.Replace(str, "\\", "\\\\", -1), "\\\\proxyPassword.exe", "", -1) + "\"\n\n"
 		data += "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\proxyPassword\\DefaultIcon]\n"
 		data += "@=\"%SystemRoot%\\system32\\url.dll,0\"\n\n"
 		data += "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\proxyPassword\\Shell]\n"
@@ -136,7 +166,7 @@ func createRegisterTable() {
 		data += "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\proxyPassword\\Shell\\open]\n"
 		data += "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\proxyPassword\\Shell\\open\\command]\n"
 		data += "@=\"" + strings.Replace(exePath, "\\", "\\\\", -1) + " "
-		data += "\\\"%1\\\" \\\"%2\\\" \\\"%3\\\" \\\"%4\\\" \\\"%5\\\"\""
+		data += "\\\"%1\\\" \\\"%2\\\" \\\"%3\\\" \\\"%4\\\" \\\"%5\\\" \\\"%6\\\"\""
 		//写入byte的slice数据
 		file.Write([]byte(data))
 		file.Close()
@@ -184,6 +214,18 @@ func createFileIfNotExist(configPath string) {
 	log.Info("Directory structure and/or file already exist or created successfully.")
 }
 
+// 文件是否存在
+func fileExists(file string) bool {
+	_, err := os.Stat(file)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return false
+}
+
 // 初始化日志
 func initLog(logPath string) {
 	log.Formatter = &logrus.JSONFormatter{}
@@ -203,16 +245,4 @@ func successMessage(message string) {
 // 失败提示
 func errorMessage(message string) {
 	w32.MessageBox(w32.HWND(uintptr(0)), message, "操作失败", w32.MB_ICONERROR|w32.MB_ICONINFORMATION)
-}
-
-// 文件是否存在
-func fileExists(file string) bool {
-	_, err := os.Stat(file)
-	if err == nil {
-		return true
-	}
-	if os.IsNotExist(err) {
-		return false
-	}
-	return false
 }
